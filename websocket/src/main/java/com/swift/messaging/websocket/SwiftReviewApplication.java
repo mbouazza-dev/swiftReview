9ee4f74237aef48f25068cea3e92cc3c69bfa2c7
mbouazza <mbouazza@etud.u-pem.fr>
2021-02-15 18:46:32 +0100

    Creation + WebSocket configuration

---
+++ websocket/src/main/java/com/swift/messaging/websocket/SwiftReviewApplication.java
@@ -0,0 +1,13 @@
+package com.swift.messaging.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftReviewApplication.class, args);
+	}
+
+}
---
+++ websocket/src/test/java/com/swift/messaging/websocket/SwiftReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swift.messaging.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
