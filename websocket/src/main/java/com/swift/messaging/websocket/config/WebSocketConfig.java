adcd528a7fb8e03826221acbcbc8d3e3b3ef0a9a
mbouazza <mbouazzapro@gmail.com>
2021-03-14 14:26:53 +0100

    Setting up the rabbitMQ brocker

--- websocket/src/main/java/com/swift/messaging/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/swift/messaging/websocket/config/WebSocketConfig.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -13,10 +16,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/ws").withSockJS();
     }
 
+    private Integer findRandomOpenPortOnAllLocalInterfaces() throws IOException {
+        try (
+                ServerSocket socket = new ServerSocket(0);
+        ) {
+            return socket.getLocalPort();
+
+        }
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+
+        // Use this for enabling a Full featured broker like RabbitMQ
+            registry.enableStompBrokerRelay("/topic")
+                    .setRelayHost("localhost")
+                    .setClientLogin("guest")
+                    .setClientPasscode("guest");
     }
+
 }
 
